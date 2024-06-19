5235e177713c320fb4b21ab4651647a827fd1d92
Carlosbueloni <carlosbueloni@gmail.com>
2024-06-18 18:55:53 -0700

    Basic implementation of finding acronym match

--- scrape-list.py
+++ scrape-list.py
@@ -3,19 +3,27 @@ from bs4 import BeautifulSoup
 import re
 
 # Specify url of the web page
-source = urlopen('https://en.wiktionary.org/wiki/Category:English_idioms').read()
+source = urlopen('https://en.wiktionary.org/w/index.php?title=Category:English_idioms&from=A').read()
 
 # Make a soup 
 soup = BeautifulSoup(source,'lxml')
 soup
 
 # Extract the plain text content from paragraphs
-phrases = []
-for phrase in soup.find_all('div', class_='mw-category-group'):
-    print(phrase.text)
+text = soup.find(attrs = {"class", 'mw-category mw-category-columns'}).text
+phrases= []
 # Extract text from paragraph headers
+for phrase in text.split('\n'):
+    phrases.append(phrase.split())
 
+def get_initials(phrase):
+    return ''.join([word[0].lower() for word in phrase])
 
+def search_for_phrase(initials):
+    for phrase in phrases:
+        if get_initials(phrase) == initials:
+            return ' '.join(phrase)
 
-print(phrases)
+    return 'Phrase for acronym not found'
 
+print(search_for_phrase('ab'))
