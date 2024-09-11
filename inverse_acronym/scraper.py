2747a3cd83108339df8c548344506d7a81899595
CarlosBueloni <carlosbueloni@gmail.com>
2024-09-11 17:58:39 -0300

    fix wiktionary url

--- inverse_acronym/scraper.py
+++ inverse_acronym/scraper.py
@@ -13,7 +13,7 @@ class Scraper():
         while True:
             req = request.copy()
             req.update(last_continue)
-            result = requests.get('https://en.wikitionary.org/w/api.php',params=req).json()
+            result = requests.get('https://en.wiktionary.org/w/api.php',params=req).json()
             if 'error' in result:
                 raise Exception(result['error'])
             if 'warnings' in result:
