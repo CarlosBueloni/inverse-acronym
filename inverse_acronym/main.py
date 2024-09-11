42eef174112b6b1eed45fbd5fe270e8b814fcb0b
CarlosBueloni <carlosbueloni@gmail.com>
2024-09-11 16:50:24 -0300

    add api access to wikitionary

--- inverse_acronym/main.py
+++ inverse_acronym/main.py
@@ -1,9 +1,9 @@
 from scraper import Scraper
 
 def main():
-    acronym = input("Enter the acronym: ")
+    #acronym = input("Enter the acronym: ")
     new_scraper = Scraper()
-    new_scraper.get_html("https://en.wiktionary.org/w/index.php?title=Category:English_idioms&from=A")
+
 
 if __name__ == "__main__":
     main()
--- inverse_acronym/scraper.py
+++ inverse_acronym/scraper.py
@@ -2,18 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 
 class Scraper():
-    def __init__(self):
-        self._r = None
-        self._soup = None
+    def __init__(self) -> None:
+        for result in self.query({'action':'query','list':'categorymembers','cmtitle':'Category:English_idioms', 'cmprop':'title','cmlimit':'max'}):
+            print(result)
 
+    def query(self, request):
+        request['action'] = 'query'
+        request['format'] = 'json'
+        last_continue = {}
+        while True:
+            req = request.copy()
+            req.update(last_continue)
+            result = requests.get('https://en.wikitionary.org/w/api.php',params=req).json()
+            if 'error' in result:
+                raise Exception(result['error'])
+            if 'warnings' in result:
+                print(result['warnings'])
+            if 'query' in result:
+                yield result['query']
+            if 'continue' not in result:
+                break
+            last_continue = result['continue']
 
-    def get_html(self, url):
-        self._r = requests.get(url)
-        self._soup = BeautifulSoup(self._r.content, "html.parser")
-        self._search_idioms()
-
-    def _search_idioms(self):
-        if self._soup is not None:
-            idioms = self._soup.find("div", class_="mw-category mw-category-columns")
-            for idiom in idioms.find_all("li"):
-                print(idiom.get_text())
